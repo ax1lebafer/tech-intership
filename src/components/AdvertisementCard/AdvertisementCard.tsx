122b08f5dcaaddee18315ba6451cc60ee5eb8cab
ax1lebafer <raul8917@gmail.com>
2024-09-07 16:42:56 +0400

    Доделал карточку товара

--- src/components/AdvertisementCard/AdvertisementCard.tsx
+++ src/components/AdvertisementCard/AdvertisementCard.tsx
@@ -1,6 +1,7 @@
 import * as React from 'react';
 import styles from './AdvertisementCard.module.css';
 import { Advertisment } from '../../../types.ts';
+import { Link } from 'react-router-dom';
 
 type AdvertisementCardProps = {
   advertisement: Advertisment;
@@ -10,7 +11,25 @@ const AdvertisementCard: React.FC<AdvertisementCardProps> = ({
 }) => {
   return (
     <div className={styles.card}>
-      <h1>{advertisement.name}</h1>
+      <img
+        className={styles.image}
+        src={advertisement.imageUrl || 'https://via.placeholder.com/250'}
+        alt="product-image"
+      />
+      <Link className={styles.title} to="/">
+        {advertisement.name}
+      </Link>
+      <p className={styles.price}>{advertisement.price} ₽</p>
+      <div className={styles.countBox}>
+        <div className={styles.viewBox}>
+          <img src="/eye.png" alt="views" />
+          <span className={styles.views}>{advertisement.views}</span>
+        </div>
+        <div className={styles.likeBox}>
+          <img src="/like.png" alt="views" />
+          <span className={styles.likes}>{advertisement.likes}</span>
+        </div>
+      </div>
     </div>
   );
 };
